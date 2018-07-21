69763b1280772602d5a9b2aae3c7e284b4e37895
Tim Fleming <tfleming26@hotmail.com>
2018-07-21 01:12:54 -0600

    Add title helper for product show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(*content)
+    unless content.empty?
+      content_for :title do
+        (content << "Met Trackify").join(" - ")
+      end
+    end
+  end
 end
--- spec/features/creating_products_spec.rb
+++ spec/features/creating_products_spec.rb
@@ -14,6 +14,12 @@ RSpec.feature "Users can create new products" do
     fill_in "Article Location",	with: "15-003"
     click_button "Add New Product"     
     
-    expect(page).to have_content "Product Information Created."  
-  end
+    expect(page).to have_content "Product Information Created."
+    
+    product = Product.find_by(article_name: "Christmas Tree")
+    expect(page.current_url).to eq product_url(product)
+
+    title = "Christmas Tree - Products - Met Trackify"
+    expect(page).to have_title title
+  end  
 end
