a611c9bc7ab6aa546da69678aa5290ce30f1889c
Tim Fleming <tfleming26@hotmail.com>
2018-08-21 09:47:25 -0600

    Add ability for only admins to set links to create and delete products and projects

---
+++ spec/features/hidden_links_spec.rb
@@ -0,0 +1,108 @@
+require "rails_helper"
+
+RSpec.feature "Users can only see the appropriate links" do
+  let(:product) { FactoryBot.create(:product) }
+  let(:project) { FactoryBot.create(:project) }
+  let(:user) { FactoryBot.create(:user) }
+  let(:admin) { FactoryBot.create(:user, :admin) }
+
+  context "anonymous users" do
+    scenario "cannot see the New Product link on index page" do
+      visit "/"
+      expect(page).not_to have_link "Add New Product"
+    end
+
+    scenario "cannot see the New Product link on show page" do
+      visit product_path(product)
+      expect(page).not_to have_link "Add New Product"
+    end
+
+    scenario "cannot see the Delete Product link" do
+      visit product_path(product)
+      expect(page).not_to have_link "Delete"
+    end
+
+    scenario "cannot see the New Project link on index page" do
+      visit "/projects"
+      expect(page).not_to have_link "Add New Project"
+    end    
+
+    scenario "cannot see the New Project link on show page" do
+      visit project_path(project)
+      expect(page).not_to have_link "Add New Project"
+    end
+
+    scenario "cannot see the Delete Project link" do
+      visit project_path(project)
+      expect(page).not_to have_link "Delete"
+    end
+  end
+
+  context "regular users" do
+    before { login_as(user) }
+
+    scenario "cannot see the New Product link on index page" do
+      visit "/"
+      expect(page).not_to have_link "Add New Product"
+    end
+
+    scenario "cannot see the New Product link on show page" do
+      visit product_path(product)
+      expect(page).not_to have_link "Add New Product"
+    end
+
+    scenario "cannot see the Delete Product link" do
+      visit product_path(product)
+      expect(page).not_to have_link "Delete"
+    end
+
+    scenario "cannot see the New Project link on index page" do
+      visit "/projects"
+      expect(page).not_to have_link "Add New Project"
+    end  
+
+    scenario "cannot see the New Project link on show page" do
+      visit project_path(project)
+      expect(page).not_to have_link "Add New Project"
+    end
+
+    scenario "cannot see the Delete Project link" do
+      visit project_path(project)
+      expect(page).not_to have_link "Delete"
+    end
+  end
+
+  context "admin users" do
+    before { login_as(admin) }
+
+    scenario "can see the New Product link on index page" do
+      visit "/"
+      expect(page).to have_link "Add New Product"
+    end
+
+    scenario "can see the New Product link on show page" do
+      visit product_path(product)
+      expect(page).to have_link "Add New Product"
+    end
+
+    scenario "can see the Delete Product link" do
+      visit product_path(product)
+      expect(page).to have_link "Delete"
+    end
+
+    scenario "can see the New Project link on index page" do
+      visit "/projects"
+      expect(page).to have_link "Add New Project"
+    end  
+
+    scenario "can see the New Project link on show page" do
+      visit project_path(project)
+      expect(page).to have_link "Add New Project"
+    end  
+
+    scenario "can see the Delete Project link" do
+      visit project_path(project)
+      expect(page).to have_link "Delete"
+    end
+  end
+end
