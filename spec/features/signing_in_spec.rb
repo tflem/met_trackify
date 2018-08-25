f5c4af042b8e3a3c1a1b2ee8ad51ac20e34c1b85
Tim Fleming <tfleming26@hotmail.com>
2018-08-25 13:14:15 -0600

    Add functionality that denies archived users the ability to log in

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -18,4 +18,20 @@ RSpec.feature "Users can sign in" do
     expect(page).to have_content "Logged In Successfully."
     expect(page).to have_content "Logged In As #{user.email}"
   end
+
+  scenario "unless they are archived" do
+    user.archive
+
+    visit "/"
+
+    within all("div.ui.menu")[0] do
+      click_link "Login"
+    end
+
+    fill_in "Email",	with: user.email
+    fill_in "Password",	with: "password"
+    click_button "Log In"
+    
+    expect(page).to have_content "Your Account Has Been Archived."
+  end
 end
