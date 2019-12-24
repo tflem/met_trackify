853f7708384c0f8f3d5e223b0bef7018b67da87c
Tim Fleming <tfleming26@hotmail.com>
2019-12-23 18:23:29 -0600

    Remove repetitive code from models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ApplicationRecord
-  validates :article_number, presence: true
-  validates :article_name, presence: true
-  validates :article_location, presence: true 
+  validates :article_number, :article_name, 
+            :article_location, presence: true 
   has_many  :product_roles, dependent: :delete_all
 
   def has_member?(user)
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,6 @@
 class Project < ApplicationRecord
-  validates :project_number, presence: true
-  validates :project_name, presence: true
-  validates :project_time, presence: true
+  validates :project_number, :project_name,
+            :project_time, presence: true
   has_many  :project_roles, dependent: :delete_all
 
   def has_member?(user)
